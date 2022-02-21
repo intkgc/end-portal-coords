ee3798da036b551b74d46c531fbe93886320c25e
Ibragim Maltsagov <ibragim837.tab@gmail.com>
2022-02-21 17:48:26 +0100

    - Improved main screen - Interface elements and variables initialized

--- app/src/main/java/com/jvmfrog/endportalcoords/MainActivity.java
+++ app/src/main/java/com/jvmfrog/endportalcoords/MainActivity.java
@@ -3,12 +3,55 @@ package com.jvmfrog.endportalcoords;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextInputLayout first_x_coord_layout, first_z_coord_layout, first_throw_angle_layout;
+    private TextInputLayout second_x_coord_layout, second_z_coord_layout, second_throw_angle_layout;
+
+    private TextInputEditText first_x_coord, first_z_coord, first_throw_angle;
+    private TextInputEditText second_x_coord, second_z_coord, second_throw_angle;
+
+    private MaterialButton calculate_coordinates_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+
+        // Set error text
+        //passwordLayout.error = getString(R.string.error)
+
+        // Clear error text
+        //passwordLayout.error = null
+    }
+
+    public void init() {
+        //TextInputLayout
+        first_x_coord_layout = findViewById(R.id.first_x_coord_layout);
+        first_z_coord_layout = findViewById(R.id.first_z_coord_layout);
+        first_throw_angle_layout = findViewById(R.id.first_throw_angle_layout);
+        second_x_coord_layout = findViewById(R.id.second_x_coord_layout);
+        second_z_coord_layout = findViewById(R.id.second_z_coord_layout);
+        second_throw_angle_layout = findViewById(R.id.second_throw_angle_layout);
+
+        //TextInputEditText
+        first_x_coord = findViewById(R.id.first_x_coord);
+        first_z_coord = findViewById(R.id.first_z_coord);
+        first_throw_angle = findViewById(R.id.first_throw_angle);
+        second_x_coord = findViewById(R.id.second_x_coord);
+        second_z_coord = findViewById(R.id.second_z_coord);
+        second_throw_angle = findViewById(R.id.second_throw_angle);
+
+        //Buttons
+        calculate_coordinates_btn = findViewById(R.id.calculate_coordinates_btn);
     }
 }
