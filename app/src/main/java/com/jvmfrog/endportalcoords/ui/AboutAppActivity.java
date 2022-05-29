6889b05280ffc38cdadc51401a0d78758b48f3b2
Ibragim Maltsagov <ibragim837.tab@gmail.com>
2022-05-29 09:50:26 +0200

    1. Updated app name to - End Portal Seeker 2. Update About App Activity

--- app/src/main/java/com/jvmfrog/endportalcoords/ui/AboutAppActivity.java
+++ app/src/main/java/com/jvmfrog/endportalcoords/ui/AboutAppActivity.java
@@ -3,6 +3,8 @@ package com.jvmfrog.endportalcoords.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 
@@ -25,13 +27,21 @@ public class AboutAppActivity extends AppCompatActivity {
 
         binding.toolbar.setNavigationOnClickListener(v -> onBackPressed());
 
-        binding.versionText.setText(getString(R.string.version) + ":" + " " + BuildConfig.VERSION_NAME + " " + "(" + BuildConfig.VERSION_CODE + ")");
-
         final ChromeCustomTabAPI chromeCustomTabAPI = new ChromeCustomTabAPI(AboutAppActivity.this);
 
+        binding.appVersionBtn.setText(getString(R.string.version) + ": " + BuildConfig.VERSION_NAME + "(" + BuildConfig.VERSION_CODE + ")");
         binding.sourceCodeBtn.setOnClickListener(v -> chromeCustomTabAPI.OpenCustomTab(AboutAppActivity.this, getString(R.string.source_code_url), R.color.white));
         binding.kirillBtn.setOnClickListener(v -> chromeCustomTabAPI.OpenCustomTab(AboutAppActivity.this, getString(R.string.kirill_url), R.color.white));
         binding.ibragimBtn.setOnClickListener(v -> chromeCustomTabAPI.OpenCustomTab(AboutAppActivity.this, getString(R.string.ibragim_url), R.color.white));
+        binding.vkGroupBtn.setOnClickListener(v -> chromeCustomTabAPI.OpenCustomTab(AboutAppActivity.this, getString(R.string.JVMFrog), R.color.white));
+        binding.otherAppsBtn.setOnClickListener(view1 -> {
+            final String appPackageName = getPackageName(); // getPackageName() from Context or Activity object
+            try {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://search?q=pub:JVMFrog")));
+            } catch (android.content.ActivityNotFoundException anfe) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.GOOGLE_PLAY))));
+            }
+        });
         binding.translateAppBtn.setOnClickListener(v -> chromeCustomTabAPI.OpenCustomTab(AboutAppActivity.this, getString(R.string.crowdin_project_url), R.color.white));
     }
 
